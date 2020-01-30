dae3cda93342d8cd205db4d9b992ff46131269ee
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2020-01-30 14:06:31 -0600

    1

---
+++ iOS Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Tip
+//
+//  Created by student on 1/30/20.
+//  Copyright © 2020 Avishek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
