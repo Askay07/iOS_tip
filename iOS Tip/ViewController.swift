9244ea056212be3efc4e85546b28c3a9b3409862
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2020-02-05 18:50:07 -0600

    5

--- iOS Tip/ViewController.swift
+++ iOS Tip/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var totalTF:UITextField!
+    @IBOutlet weak var TipLBL:UILabel!
+    @IBOutlet weak var TaxLBL:UILabel!
+    @IBOutlet weak var RateLBL:UILabel!
+    @IBOutlet weak var grandTotal:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func CALCULATE(sender:UIButton){
+           
+           if let total = Double(totalTF.text!) {
+               
+               
+               let tax = ((7/100) * total)
+               let tip = (20/100) * total
+               let grandTotal = (total + tax + tip)
+            
+               tipLBL.text = String(format:"%.2f", tip)
+               tipLBL.tot_tax = String(format:"%.2f", tot_tax)
+               tipLBL.tot_tax = String(format:"%.2f", tot_tax)
+    
 
 
 }
 
+
+
+    else{
+        
+        //display an alert
+        
+        let ac = UIAlertController(title: "It is an illegeal Year", message: "Please enter a valid value", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel)
+        ac.addAction(action)
+        self.present(ac, animated: true)
+        
+        print("Nice try bro")
+        
+        
+        
+    }
+
+}
